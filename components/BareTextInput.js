255cd41bf95356733e506c0eaa1ba9b85bb2ed5d
tranzystor <tranzystor.github@gmail.com>
2018-11-16 16:45:34 +0100

    bare text input

--- components/BareTextInput.js
+++ components/BareTextInput.js
@@ -4,15 +4,23 @@ import { StyleSheet, TextInput, View } from 'react-native';
 const inactiveBorderColor = '#464646';
 const mainColor = '#1378ba'; // blue 
 
-export default class BareTextInput extends Component{
+export default class BareTextInput extends Component {
     state = {
-        inputFocus: false
+        inputFocus: false,
+        text: ''
     }
 
     toggleFocus = () => {
         this.setState(prevState => {
             return { inputFocus: !prevState.inputFocus }
         });
+    }
+
+    onChangeText = text =>{
+        this.setState({text});
+        if(this.props.textChanged){
+            this.props.textChanged(text);
+        }
     }
 
     render() {
@@ -31,20 +39,31 @@ export default class BareTextInput extends Component{
                             onBlur={this.toggleFocus} 
                             style={styles.input} 
                             selectionColor={mainColor}
-                            placeholder={placeholder} />
+                            placeholder={placeholder}
+                            onChangeText={this.onChangeText}
+                            value={this.state.text}
+                            style={styles.input}
+                            />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    input: {
+        padding: 1,
+        margin: 2,
+    },
     wrapper: {
-        height: 20,
+        height: 25,
+        padding: 0,
         borderBottomWidth: 1,
         borderBottomColor: inactiveBorderColor,
+        marginBottom: 3
     },
     wrapperWithFocusedInput: {
         borderBottomWidth: 2,
         borderBottomColor: mainColor,
+        marginBottom: 3
     }
 });
--- newTaskPage/index.js
+++ newTaskPage/index.js
@@ -1,41 +1,27 @@
 
 
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 import BareTextInput from '../components/BareTextInput';
 import LabelledTextInput from '../components/LabelledTextInput';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class NewTaskPage extends Component {
 
+  state = {
+    text: 'default'
+  }
+
+  textChanged = (text) => {
+    this.setState({text});
+  }
+
   render() {
     return (
-      <View style={{paddingLeft: 15, paddingRight: 15}}>
-        <Text>icon <Icon name="date-range" color="black" size={20} /> icon</Text>
-        <Text>icon <Icon name="cancel" color="black" size={20} /> icon</Text>
-        <Text>icon <Icon name="access-time" color="black" size={20} /> icon</Text>
-        <BareTextInput placeholder='abc' />
-        <BareTextInput placeholder='abc' />
-        <View style={{marginTop: 25}}>
-          <LabelledTextInput placeholder='abc' />
-        </View>
-        <View style={{marginTop: 25}}>
-          <LabelledTextInput label='To do description' placeholder='abc' />
-        </View>
-        <View style={{marginTop: 25}}>
-          <LabelledTextInput label='Date' placeholder='abc' />
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'azure'}}>
-          {/* <BareTextInput placeholder='here' label='' /> */}
-          {/* <BareTextInput placeholder='here' label='' /> */}
-          <LabelledTextInput placeholder='here' />
-          <View style={{backgroundColor: 'red'}}>
-            <Icon name="date-range" color="black" size={20} />
-          </View>
-        </View>
-        <View style={{marginTop: 25, flex: 1}}>
-          <LabelledTextInput label='Date' placeholder='after' />
-        </View>
+      <View style={{paddingLeft: 15, paddingRight: 15, paddingTop: 10}}>
+        <BareTextInput placeholder='placeholder' textChanged={this.textChanged} />
+        <BareTextInput placeholder='second' />
+        <Text>{this.state.text}</Text>
       </View>
     );
   }
